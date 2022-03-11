bf532a772bdbe30ae1722314fd4a8d5ea5d89095
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-11 16:32:57 +0100

    Provo a stampare un console.log dentro method

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
+/*Partendo dai file nello starter implementare la funzione dei bottoni (next e previous) dello slider usando Vue (prima però create la struttura dati).
+
+Bonus:
+1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+Consiglio del giorno:
+- regola d'oro: riciclare ovunque possibile! E il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe ;)
+Buon lavoro e buon divertimento!*/
+
 
 const app = new Vue ({
     el: '#root',
+    test: 3,
     data:{
         arrData:[
             {
@@ -28,7 +39,14 @@ const app = new Vue ({
                 countryImg:'05.jpg',
                 countryText:'Et temporibus voluptatum suscipitempore      aliquid deleniti aut veniam inventoreligendi ex ad ullam,',
             },
-        ]    
-    }
+        ], 
 
+        
+    },
+    methods: {
+        console.log(this.arrData);
+    },
 });
+
+
+
